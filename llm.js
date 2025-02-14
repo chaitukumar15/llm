d2838282e1b2fc6cf13b8f23c73610a0ca3ccb84
chaitukumar15 <108114098+chaitukumar15@users.noreply.github.com>
2025-02-14 17:45:09 +0530

    Add files via upload

---
+++ llm.js
@@ -0,0 +1,45 @@
+
+
+var express= require("express");
+
+var app=express();
+
+app.use(express.json())
+
+
+app.get("/",(req,res)=>{
+
+
+    fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer 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",
+          "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
+          "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "model": "google/gemini-2.0-flash-lite-preview-02-05:free",
+          "messages": [
+            {"role": "user", "content": req.body.msg}
+          ],
+          "top_p": 1,
+          "temperature": 0.4,
+          "repetition_penalty": 1
+        })
+      }).then((val)=>{
+        return val.json()
+      }).then((val)=>{
+
+        res.send(val)
+
+      })
+
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("hi server stareted");
+    
+})
