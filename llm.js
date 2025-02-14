e131b34b313efacea65cb3a73d79a1cfa57f2dd8
chaitukumar15 <108114098+chaitukumar15@users.noreply.github.com>
2025-02-14 17:54:18 +0530

    Update llm.js

--- llm.js
+++ llm.js
@@ -13,7 +13,7 @@ app.get("/",(req,res)=>{
     fetch("https://openrouter.ai/api/v1/chat/completions", {
         method: "POST",
         headers: {
-          "Authorization": "Bearer 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",
+          "Authorization": "Bearer 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",
           "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
           "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
           "Content-Type": "application/json"
